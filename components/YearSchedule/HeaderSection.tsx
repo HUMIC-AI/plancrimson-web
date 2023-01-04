754d96a5baba1df19c61e3272fa06a3eade75558
Alexander Cai <alexandercai@outlook.com>
2023-01-03 18:29:35 -0700

    move options into cog

--- components/YearSchedule/HeaderSection.tsx
+++ components/YearSchedule/HeaderSection.tsx
@@ -1,13 +1,16 @@
+import { Menu } from '@headlessui/react';
 import React, { useMemo } from 'react';
 import {
   FaAngleDoubleLeft,
   FaArrowsAltH,
+  FaCog,
 } from 'react-icons/fa';
+import { useModal } from 'src/context/modal';
 import { DownloadPlan } from '../../shared/types';
 import { allTruthy } from '../../shared/util';
 import { Planner, Schedules, Settings } from '../../src/features';
 import { downloadJson, useAppDispatch, useAppSelector } from '../../src/hooks';
-import UploadPlan from '../UploadPlan';
+import UploadForm from '../UploadPlan';
 import CardExpandToggler from './CardExpandToggler';
 import { WithResizeRef } from './PlanningSection';
 import { SemesterDisplayProps } from './SemesterDisplay';
@@ -17,28 +20,8 @@ import { SemesterDisplayProps } from './SemesterDisplay';
  */
 export default function HeaderSection({ resizeRef, columns }: WithResizeRef & { columns: SemesterDisplayProps[] }) {
   const dispatch = useAppDispatch();
-  const userSchedules = useAppSelector(Schedules.selectSchedules);
-  const chosenSchedules = useAppSelector(Settings.selectChosenSchedules);
   const showReqs = useAppSelector(Planner.selectShowReqs);
   const semesterFormat = useAppSelector(Planner.selectSemesterFormat);
-  const sampleSchedule = useAppSelector(Planner.selectSampleSchedule);
-
-  const downloadData: DownloadPlan = {
-    id: semesterFormat === 'sample'
-      ? sampleSchedule!.id
-      : Math.random().toString(16).slice(2, 18),
-    schedules: allTruthy(
-      columns.map(({ chosenScheduleId }) => (chosenScheduleId ? userSchedules[chosenScheduleId] : null)),
-    ),
-  };
-
-  const totalCourses = useMemo(
-    () => Object.values(chosenSchedules).reduce(
-      (acc, scheduleId) => acc + ((scheduleId && userSchedules[scheduleId]?.classes.length) || 0),
-      0,
-    ),
-    [userSchedules, chosenSchedules],
-  );
 
   return (
     <div className="relative text-white">
@@ -53,18 +36,13 @@ export default function HeaderSection({ resizeRef, columns }: WithResizeRef & { 
         </button>
       )}
 
-      <div className="flex flex-col items-center justify-center gap-4">
+      <OptionsMenu columns={columns} />
 
-        <span className="whitespace-nowrap">
-          Total courses:
-          {' '}
-          {totalCourses}
-          {' '}
-          / 32
-        </span>
+      <div className="flex flex-col items-center justify-center gap-4">
 
         <div className="flex flex-wrap items-center justify-center gap-4 text-sm">
           <CardExpandToggler />
+
           {semesterFormat !== 'sample' && (
             <button
               type="button"
@@ -82,9 +60,63 @@ export default function HeaderSection({ resizeRef, columns }: WithResizeRef & { 
                 : 'Only selected schedules'}
             </button>
           )}
+
+          <div
+            ref={resizeRef}
+            className="flex w-24 min-w-[96px] max-w-full resize-x justify-center overflow-auto rounded bg-gray-600 py-1"
+          >
+            <FaArrowsAltH />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function OptionsMenu({ columns }: { columns: SemesterDisplayProps[] }) {
+  const dispatch = useAppDispatch();
+  const userSchedules = useAppSelector(Schedules.selectSchedules);
+  const semesterFormat = useAppSelector(Planner.selectSemesterFormat);
+  const sampleSchedule = useAppSelector(Planner.selectSampleSchedule);
+  const chosenSchedules = useAppSelector(Settings.selectChosenSchedules);
+
+  const { showContents } = useModal();
+
+  const totalCourses = useMemo(
+    () => Object.values(chosenSchedules).reduce(
+      (acc, scheduleId) => acc + ((scheduleId && userSchedules[scheduleId]?.classes.length) || 0),
+      0,
+    ),
+    [userSchedules, chosenSchedules],
+  );
+
+  const downloadData: DownloadPlan = {
+    id: semesterFormat === 'sample'
+      ? sampleSchedule!.id
+      : Math.random().toString(16).slice(2, 18),
+    schedules: allTruthy(
+      columns.map(({ chosenScheduleId }) => (chosenScheduleId ? userSchedules[chosenScheduleId] : null)),
+    ),
+  };
+
+  return (
+    <Menu as="div" className="absolute top-1 right-2">
+      <Menu.Button>
+        <FaCog className="transition-opacity hover:opacity-50" />
+      </Menu.Button>
+      <Menu.Items className="absolute top-full right-0 z-10 mt-2 space-y-1  rounded bg-white p-2 text-sm text-black shadow-xl">
+        <span className="whitespace-nowrap text-gray-600">
+          Total courses:
+          {' '}
+          {totalCourses}
+          {' '}
+          / 32
+        </span>
+
+        <Menu.Item>
           <button
             type="button"
-            className="interactive underline"
+            className="transition-opacity hover:opacity-50"
             onClick={() => downloadJson(
               semesterFormat === 'sample'
                 ? `Sample ${sampleSchedule?.id} - Plan Crimson`
@@ -94,37 +126,41 @@ export default function HeaderSection({ resizeRef, columns }: WithResizeRef & { 
           >
             Download all
           </button>
-
-          <UploadPlan />
-
+        </Menu.Item>
+        <Menu.Item>
+          <button
+            type="button"
+            onClick={() => showContents({
+              content: <UploadForm />,
+              title: 'Upload plan',
+            })}
+            className="transition-opacity hover:opacity-50"
+          >
+            Upload plan
+          </button>
+        </Menu.Item>
+        <Menu.Item>
           {semesterFormat !== 'sample' && (
-            <button
-              type="button"
-              className="interactive underline"
-              onClick={() => {
-                // eslint-disable-next-line no-restricted-globals
-                const yn = confirm(
-                  'Are you sure? This will remove all courses from all selected schedules!',
-                );
-                if (yn) {
-                  allTruthy(
-                    Object.values(chosenSchedules).map((id) => (id ? userSchedules[id] : null)),
-                  ).forEach((schedule) => dispatch(Schedules.clearSchedule(schedule.id)));
-                }
-              }}
-            >
-              Reset all
-            </button>
+          <button
+            type="button"
+            className="transition-opacity hover:opacity-50"
+            onClick={() => {
+            // eslint-disable-next-line no-restricted-globals
+              const yn = confirm(
+                'Are you sure? This will remove all courses from all selected schedules!',
+              );
+              if (yn) {
+                allTruthy(
+                  Object.values(chosenSchedules).map((id) => (id ? userSchedules[id] : null)),
+                ).forEach((schedule) => dispatch(Schedules.clearSchedule(schedule.id)));
+              }
+            }}
+          >
+            Reset all
+          </button>
           )}
-        </div>
-
-        <div
-          ref={resizeRef}
-          className="flex w-24 min-w-[96px] max-w-full resize-x justify-center overflow-auto rounded bg-gray-600 py-1"
-        >
-          <FaArrowsAltH />
-        </div>
-      </div>
-    </div>
+        </Menu.Item>
+      </Menu.Items>
+    </Menu>
   );
 }
