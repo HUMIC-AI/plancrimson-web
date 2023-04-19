afcc6ec5526844c4570fd3f9bbfb21fe090f7bb7
Alexander Cai <alexandercai@outlook.com>
2023-04-19 00:54:38 +0000

    remove server, scripts, and shared directories

--- scripts/README.md
+++
@@ -1,5 +0,0 @@
-# Scripts for Plan Crimson
-
-API calls to Meilisearch are handled through Postman.
-
-Otherwise, just run `yarn script` for an interactive CLI.
--- server/initFirebase.ts
+++
@@ -1,8 +0,0 @@
-import { initializeApp, getApps, applicationDefault } from 'firebase-admin/app';
-
-if (getApps().length === 0) {
-  initializeApp({
-    credential: applicationDefault(),
-    projectId: 'harvard-concentration-planner',
-  });
-}
