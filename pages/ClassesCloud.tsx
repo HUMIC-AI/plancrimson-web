36a28b008ebb04db36ec4d858cc88fe2b11fbce2
Alexander Cai <alexandercai@outlook.com>
2023-04-20 03:14:57 -0400

    change points into spheres

--- pages/ClassesCloud.tsx
+++ pages/ClassesCloud.tsx
@@ -10,32 +10,13 @@ export function ClassesCloud() {
 
 
   useEffect(() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
-    const renderer = new THREE.WebGLRenderer({ canvas: canvas.current! });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    const { scene, camera, renderer } = createScene(canvas.current!);
+    const controls = createControls(camera, renderer);
+    const points = getPoints();
+    scene.add(...points);
 
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.minDistance = 1;
-    controls.maxDistance = 80;
-    controls.autoRotate = true;
-
-    const points = tsne3d.map((point) => new THREE.Vector3(point[0], point[1], point[2]));
-
-    const material = new THREE.PointsMaterial({ vertexColors: true });
-    const geometry = new THREE.BufferGeometry().setFromPoints(points);
-
-    const color = new THREE.Color();
-    const subjectColors = Object.fromEntries(subjectNames.map((subject, i) => {
-      color.setHSL(i / subjectNames.length, 1, 0.5);
-      return [subject, [color.r, color.g, color.b]] as const;
-    }));
-    const colors = courses.flatMap((c) => subjectColors[c.subject] ?? [0, 0, 0]);
-    geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
-
-    const scatterPlot = new THREE.Points(geometry, material);
-    scene.add(scatterPlot);
+    const light = new THREE.PointLight(0xffffff, 1, 100);
+    scene.add(light);
 
     function animate() {
       requestAnimationFrame(animate);
@@ -62,3 +43,48 @@ export function ClassesCloud() {
   );
 }
 
+function createScene(canvas: HTMLCanvasElement) {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 5;
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  return { scene, camera, renderer };
+}
+
+function createControls(camera: THREE.PerspectiveCamera, renderer: THREE.WebGLRenderer) {
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.minDistance = 1;
+  controls.maxDistance = 80;
+  controls.autoRotate = true;
+  return controls;
+}
+
+function getCourseColorsBySubject() {
+  const color = new THREE.Color();
+  const subjectColors = Object.fromEntries(subjectNames.map((subject, i) => {
+    color.setHSL(i / subjectNames.length, 1, 0.5);
+    return [subject, [color.r, color.g, color.b]] as const;
+  }));
+  return subjectColors;
+}
+
+function getPoints() {
+  const points = [];
+  const colors = getCourseColorsBySubject();
+  for (let i = 0; i < courses.length; i += 1) {
+    const geometry = new THREE.SphereGeometry(0.1, 12);
+    const color = new THREE.Color();
+    if (courses[i].subject in colors) {
+      color.setRGB(...colors[courses[i].subject]);
+    } else {
+      color.setRGB(0.5, 0.5, 0.5);
+    }
+    const material = new THREE.MeshBasicMaterial({ color });
+    const sphere = new THREE.Mesh(geometry, material);
+    const pos = tsne3d[i];
+    sphere.position.set(pos[0], pos[1], pos[2]);
+    points.push(sphere);
+  }
+  return points;
+}
