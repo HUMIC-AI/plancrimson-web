639c4474d46986ff97f1524ab0b37362b0ebee1a
Alexander Cai <alexandercai@outlook.com>
2024-04-20 01:53:07 -0400

    style: add small CuteSwitch

--- components/Utils/CuteSwitch.tsx
+++ components/Utils/CuteSwitch.tsx
@@ -1,9 +1,10 @@
 import { Switch } from '@headlessui/react';
 import { classNames } from '../../src/utils/styles';
 
-export function CuteSwitch({ enabled, onChange }: {
+export function CuteSwitch({ enabled, onChange, size = 'md' }: {
   enabled: boolean;
   onChange: (checked: boolean) => void;
+  size?: 'sm' | 'md';
 }): JSX.Element {
   // Code from https://headlessui.dev/react/switch
   return (
@@ -12,7 +13,8 @@ export function CuteSwitch({ enabled, onChange }: {
       onChange={onChange}
       className={classNames(
         enabled ? 'bg-blue-primary/80' : 'bg-blue-secondary/80',
-        'relative inline-flex items-center h-[28px] w-[64px]',
+        'relative inline-flex items-center',
+        size === 'md' ? 'h-[28px] w-[64px]' : 'h-[24px] w-[48px]',
         'border-2 border-transparent rounded-full cursor-pointer',
         'transition-colors ease-in-out duration-500',
         'focus:outline-none focus-visible:ring-2 focus-visible:ring-secondary/80',
@@ -22,8 +24,10 @@ export function CuteSwitch({ enabled, onChange }: {
       <span
         aria-hidden="true"
         className={classNames(
-          enabled ? 'translate-x-9 bg-gray-secondary' : 'translate-x-0 bg-gray-primary',
-          'pointer-events-none h-[24px] w-[24px] rounded-full',
+          enabled && (size === 'md' ? 'translate-x-9' : 'translate-x-6'),
+          enabled ? 'bg-gray-secondary' : 'translate-x-0 bg-gray-primary',
+          'pointer-events-none rounded-full',
+          size === 'md' ? 'h-[24px] w-[24px]' : 'h-[20px] w-[20px]',
           'shadow-lg transform ring-0',
           'transition ease-in-out duration-200',
         )}
