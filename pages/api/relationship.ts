5e8e9e66850d5f26b43e555963668a23cb9619fb
Alexander Cai <alexandercai@outlook.com>
2024-04-29 19:50:04 -0400

    fix: improve prompt

--- pages/api/relationship.ts
+++ pages/api/relationship.ts
@@ -16,11 +16,11 @@ export default async function handler(
       messages: [
         {
           role: 'system',
-          content: 'You are an advisor working at Harvard College. You\'re very knowledgeable about the courses offered at Harvard. A student has come to you with a question about two courses they want to compare. Provide a brief and insightful response. Be kind, excited, and encouraging! Try to spark curiosity and ask the student thought-provoking questions. Be concise!',
+          content: 'You are an advisor working at Harvard College. You\'re very knowledgeable about the courses offered at Harvard. A student has come to you with two courses they want to compare. Provide a brief and insightful response. Avoid summarizing the courses individually; only compare and contrast them. Be kind, excited, and encouraging! Try to spark curiosity and ask the student thought-provoking questions. Be concise!',
         },
         {
           role: 'user',
-          content: `Could you please describe the relationship between these two courses?
+          content: `Compare these two courses:
 
 ${src.HU_SUBJ_CATLG_NBR}: ${src.Title} (taught by ${src.IS_SCL_DESCR_IS_SCL_DESCRL})
 ${src.textDescription}
